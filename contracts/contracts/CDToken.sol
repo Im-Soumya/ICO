0f928384a73ad72ea8e0a3441159ed82013394bd
Soumya <aeie2019@rcciit.org.in>
2022-07-19 10:53:46 +0530

    added ui

---
+++ contracts/contracts/CDToken.sol
@@ -0,0 +1,58 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IMyNFT.sol";
+
+contract CDToken is ERC20, Ownable {
+    IMyNFT MyNFT;
+
+    uint256 public constant maxTokens = 10000 * 10**18;
+    uint256 public constant pricePerToken = 0.001 ether;
+    uint256 public constant tokensPerNFT = 10 * 10**18;
+
+    mapping(uint256 => bool) public tokensClaimed;
+
+    constructor(address nftContractAddress) ERC20("Crypto Devs", "CD") {
+        MyNFT = IMyNFT(nftContractAddress);
+    }
+
+    function publicMint(uint256 amount) public payable {
+        require(msg.value >= amount * pricePerToken, "Didnot send enough eth");
+        require(
+            (totalSupply() + (amount * 10**18)) <= maxTokens,
+            "Max limit reached"
+        );
+        _mint(msg.sender, amount * 10**18);
+    }
+
+    function claimToken() public payable {
+        uint256 balance = MyNFT.balanceOf(msg.sender);
+        require(balance > 0, "You donot have any Crypto Devs NFT");
+
+        uint256 amount = 0;
+        for (uint256 index = 0; index < balance; index++) {
+            uint256 tokenId = MyNFT.tokenOwnerByIndex(msg.sender, index);
+
+            if (!tokensClaimed[tokenId]) {
+                amount += 1;
+                tokensClaimed[tokenId] = true;
+            }
+
+            require(amount > 0, "You have already claimed your tokens");
+            _mint(msg.sender, amount * tokensPerNFT);
+        }
+    }
+
+    function withdraw() public onlyOwner {
+        address _owner = owner();
+        (bool call, ) = _owner.call{value: address(this).balance}("");
+        require(call, "Failed to withdraw");
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+}
